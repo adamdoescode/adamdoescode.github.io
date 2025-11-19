a0ecab26b89260f96c35bc19cb9d6982ad80c5d7
AdamG <grey.ham101@gmail.com>
2025-11-19 21:05:30 +0800

    working docker compose and cleanup

--- _site/test_webpage.py
+++
@@ -1,9 +0,0 @@
-'''
-Tests for the webpage.
-'''
-
-from subprocess import run
-
-def test_jekyll_build():
-    '''Test that the Jekyll build succeeds.'''
-    assert run(['jekyll', 'build']).returncode == 0
